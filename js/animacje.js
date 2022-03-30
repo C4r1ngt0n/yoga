b5b3cf40f37e2fde4829963a438ae4b4dbc8c628
C4r1ngt0n <100119195+C4r1ngt0n@users.noreply.github.com>
2022-03-30 14:22:02 +0200

    Add files via upload

--- js/animacje.js
+++ js/animacje.js
@@ -1,15 +1,35 @@
-let element = document.querySelectorAll( '.animacja' );
+//let element = document.querySelectorAll( '.animacja' );
 
-for (let i=0; i<element.length; i++) {
+//for (let i=0; i<element.length; i++) {
 
-	window.addEventListener( 'scroll', function f(e) {
-		var offset = element[i].getBoundingClientRect();
+//	window.addEventListener( 'scroll', function f(e) {
+//		var offset = element[i].getBoundingClientRect();
 		
-		if ( window.scrollY >= offset.bottom) {
-			element[i].classList.add( 'animation' );
+//		if ( window.scrollY >= offset.bottom) {
+//			element[i].classList.add( 'animation' );
 			
-			window.removeEventListener( 'scroll', f );
-		}
-	} );
+//			window.removeEventListener( 'scroll', f );
+//		}
+//	} );
 	  
-} 
+//} 
+
+const element = document.querySelectorAll( 'img' );
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+		for (let i=0; i<element.length; i++) {
+        entry.target.classList.add('animate__animated','animate__zoomIn');
+		return
+		}
+        
+      }
+    });
+  });
+  for (let i=0; i<element.length; i++) {
+  observer.observe(document.querySelector('.animacja'+[i]));
+  }
